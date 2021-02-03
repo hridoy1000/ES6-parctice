3c567608e1ac0983438d0c49ed8efbaa042d29a2
hridoy1000 <hdas201278@bscse.uiu.ac.bd>
2021-02-03 22:28:49 +0600

    last

---
+++ array-slice.js
@@ -0,0 +1,11 @@
+const nums = [1,2,3,4,5,6,7,8];
+
+const part = nums.slice(2, 5);
+const remove = nums.splice(2,2,45,56);
+// console.log(part);
+
+// console.log(remove);
+// console.log(nums);
+
+const together = nums.join("ami");
+console.log(together);
